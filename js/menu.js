c7a582801841a954b13f6676b71dd4aea7f6fd68
GG4zzaneo <gius3ppe95@gmail.com>
2017-11-22 00:46:21 +0000

    Add files via upload
    - Burger restyled.
    - Added navigation drop-in on burger toggle.
    - Added JavaScript menu on-click.
    - Small positional changes.
    - Fixed semantic tags.

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+"use strict";
+
+(function(){
+    
+  let burger = document.getElementById("burger-menu");
+
+  let nav = document.getElementsByTagName("nav")[0];
+
+  burger.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    if (nav.classList.contains("toggle-nav")){
+      nav.classList.remove("toggle-nav");
+    } else {
+      nav.classList.add("toggle-nav");
+    }
+    
+    
+  }
+    
+})()
