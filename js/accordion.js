af0abdffd918069a4cc7717793034dc80cbbfa96
GG4zzaneo <gius3ppe95@gmail.com>
2017-11-28 23:40:49 +0000

    animation added
    plus and minus character replaced with span tags
    animation added to accordion icon
    minor display fixes, such as img tags and text properties

--- js/accordion.js
+++ js/accordion.js
@@ -4,24 +4,38 @@
 
   for (let i = 0; i < accordions.length; i++) { 
     //Add on click event to all accordion containers
-    accordions[i].addEventListener("click", function(e){ toggleAccordion(e); });    
+    accordions[i].addEventListener("click", function(e){ toggleAccordion(e); }, false);    
   }
   
   function toggleAccordion(e) {
     //Store click element
-    let acc = e.target;
     
-    acc.classList.toggle("active");
+    let acc, panel;
+      
+    console.log(e.path);
     
-    let panel = acc.nextElementSibling;
-    
-    console.log(panel);
+    //length - 2 as e.path contains window which does not have a classList property to read
+    for (let i = 0; i < e.path.length - 2; i++){
+      
+      if (e.path[i].classList.contains("accordion")) {
+      
+        acc = e.path[i];
         
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = `${panel.scrollHeight}px`;
-    } 
+        console.log(e.path[i]);
+
+        acc.parentElement.classList.toggle("active");
+
+        panel = acc.nextElementSibling;
+
+        if (panel.style.maxHeight){
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = `${panel.scrollHeight}px`;
+        } 
+        //Once correct element found end loop
+        break;
+      }
+    }
   }
   
 })()
