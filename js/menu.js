a6e48d47a989920b6beef96caaed3e536d6b92cb
GG4zzaneo <gius3ppe95@gmail.com>
2017-11-30 22:56:29 +0000

    Nav update
    Animation added to nav burger
    Css files compressed

--- js/menu.js
+++ js/menu.js
@@ -11,8 +11,10 @@
   function toggleMenu() {
     if (nav.classList.contains("toggle-nav")){
       nav.classList.remove("toggle-nav");
+      burger.classList.remove("active");
     } else {
       nav.classList.add("toggle-nav");
+      burger.classList.add("active");
     }
   }
   
