f2669ac9b7e94bf03b46d5402e4aaffb5683b759
GG4zzaneo <gius3ppe95@gmail.com>
2017-12-07 21:37:59 +0000

    Nav closing feature
    Ability to close nav by clicking on the rest of the page

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,42 @@
 "use strict";
 
-(function(){
-    
-  let burger = document.getElementById("burger-menu");
+(function () {
 
-  let nav = document.getElementsByTagName("nav")[0];
-  
-  burger.addEventListener("click", toggleMenu);
-  
-  function toggleMenu() {
-    if (nav.classList.contains("toggle-nav")){
-      nav.classList.remove("toggle-nav");
-      burger.classList.remove("active");
-    } else {
-      nav.classList.add("toggle-nav");
-      burger.classList.add("active");
-    }
-  }
-  
-  
+
+	let body = document.getElementsByTagName("body")[0];
+	let burger = document.getElementById("burger-menu");
+
+	let nav = document.getElementsByTagName("nav")[0];
+
+	body.addEventListener("click", function (e) {
+		toggleMenu(e);
+	});
+
+	function toggleMenu(e) {
+		console.log(e.target);
+
+		if (e.target.id === "burger-menu" || e.target.classList.contains("burger-line")) {
+			console.log("buger or burger line");
+			
+			if (nav.classList.contains("toggle-nav")) {
+				nav.classList.remove("toggle-nav");
+				burger.classList.remove("active");
+			} else {
+				nav.classList.add("toggle-nav");
+				burger.classList.add("active");
+			}
+		} else if (e.target.tagName.toLocaleLowerCase() === "nav") {
+			console.log("nav clicked, do nothing!");
+		} else if (nav.classList.contains("toggle-nav")) {
+			console.log("nav.classList.contains(toggle-nav)");
+			console.log(e.target.tagName);
+			
+			nav.classList.remove("toggle-nav");
+			burger.classList.remove("active");
+		}
+
+
+	}
+
+
 })()
